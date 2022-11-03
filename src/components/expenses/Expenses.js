fcdd0a17ef85f0d93088ff31fd95f5faee1e3b53
TheClerici <enrique_cle97@hotmail.com>
2022-11-03 16:24:23 -0700

    final chart bar

--- src/components/expenses/Expenses.js
+++ src/components/expenses/Expenses.js
@@ -4,6 +4,7 @@ import ExpensesList from "./ExpensesList";
 import Card from "../UI/Card";
 import "./Expenses.css";
 import ExpensesFilter from "./ExpensesFilter";
+import ExpensesChart from "./ExpensesChart";
 
 function Expenses(props) {
   const [filteredYear, setFilteredYear] = useState("2022");
@@ -25,6 +26,7 @@ function Expenses(props) {
           selected={filteredYear}
           onChangeFilter={filterChangeHandler}
         />
+        <ExpensesChart expenses={filteredExpenses}/>
         <ExpensesList items={filteredExpenses}/>
       </Card>
     </div>
